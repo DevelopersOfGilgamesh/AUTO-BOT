f9fd1216541a75800926b44028c8db2a7cf5425a
Developer of Zassho Gilgamesh <korisawarezero@gmail.com>
2024-08-17 18:32:50 +0800

    Update help.js

--- script/help.js
+++ script/help.js
@@ -91,7 +91,7 @@ module.exports.handleEvent = async function({
     messageID,
     body
   } = event;
-  const message = prefix ? 'This is my prefix: ' + prefix : "Sorry i don't have prefix";
+  const message = prefix ? '🛸 Prefix: ' + prefix : "I don't have prefix";
   if (body?.toLowerCase().startsWith('prefix')) {
     api.sendMessage(message, threadID, messageID);
   }
